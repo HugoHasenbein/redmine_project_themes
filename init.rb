9a4d54eeaae02f39f620f0c3c48758faea7db417
Dr. Stephan Wenzel <stephan.wenzel@drwpatent.de>
2019-09-04 19:49:40 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-# Redmine plugin for provides project specific themes
+# Redmine plugin for providing project specific themes
 #
 # Copyright © 2019 Stephan Wenzel <stephan.wenzel@drwpatent.de>
 #
