6fbe1bff6fc7aa7bd845fa2c0f888f09a7ddaed5
Dr. Stephan Wenzel <stephan.wenzel@drwpatent.de>
2020-01-12 11:00:10 +0100

    corrected French accents

--- init.rb
+++ init.rb
@@ -35,7 +35,11 @@
 # 1.0.3
 #  - added query for theme on projects page
 # 
-# 1.1.0 - complete code revamp and simplification
+# 1.1.0 
+#  - complete code revamp and simplification
+#
+# 1.1.1 
+#  - correction of French accents
 #
 
 require 'redmine'
@@ -44,7 +48,7 @@ Redmine::Plugin.register :redmine_project_themes do
   name 'Redmine Project Themes'
   author 'Stephan Wenzel'
   description 'This plugin provides project specific themes'
-  version '1.1.0'
+  version '1.1.1'
   url 'https://github.com/HugoHasenbein/redmine_project_themes'
   author_url 'https://github.com/HugoHasenbein/redmine_project_themes'
   requires_redmine version_or_higher: '3.4'
